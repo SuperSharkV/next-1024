a5b33fb0facea0ffeff6fc2b5e758ec289f6711e
乌鸦 <lonesome_boy@126.com>
2025-05-17 22:18:45 +0800

    debug

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -10,10 +10,10 @@ export default function Footer() {
           <div>{` • `}</div>
           <div>{`© ${new Date().getFullYear()}`}</div>
         </div>
-        <div className="mb-8 text-sm text-gray-500 dark:text-gray-400">
-          Powered by{' '}
+        <div className="mb-8 flex text-sm text-gray-500 dark:text-gray-400">
+          <div>{`Powered by `}</div>
           <Link href="https://github.com/timlrx/tailwind-nextjs-starter-blog">
-            Tailwind Nextjs Theme
+            Tailwind Nextjs
           </Link>
         </div>
       </div>
